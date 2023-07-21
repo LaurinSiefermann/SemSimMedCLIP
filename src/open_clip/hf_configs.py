e0edc50aa7427c75591e41d17e981d53f0cb427f
LaurinSiefermann <siefermann.laurin@gmail.com>
2023-07-21 09:11:41 +0000

    added config for custom text model

--- src/open_clip/hf_configs.py
+++ src/open_clip/hf_configs.py
@@ -1,24 +1,35 @@
 # HF architecture dict:
 arch_dict = {
-  "roberta": {
-    "config_names": {
-      "context_length": "max_position_embeddings",
-      "vocab_size": "vocab_size",
-      "width": "hidden_size",
-      "heads": "num_attention_heads",
-      "layers": "num_hidden_layers",
+    "roberta": {
+        "config_names": {
+            "context_length": "max_position_embeddings",
+            "vocab_size": "vocab_size",
+            "width": "hidden_size",
+            "heads": "num_attention_heads",
+            "layers": "num_hidden_layers",
+        },
+        "pooler": "mean_pooler",
     },
-    "pooler": "mean_pooler",
-  },
-  # https://huggingface.co/docs/transformers/model_doc/xlm-roberta#transformers.XLMRobertaConfig
-  "xlm-roberta": {
-    "config_names": {
-      "context_length": "max_position_embeddings",
-      "vocab_size": "vocab_size",
-      "width": "hidden_size",
-      "heads": "num_attention_heads",
-      "layers": "num_hidden_layers",
+    # https://huggingface.co/docs/transformers/model_doc/xlm-roberta#transformers.XLMRobertaConfig
+    "xlm-roberta": {
+        "config_names": {
+            "context_length": "max_position_embeddings",
+            "vocab_size": "vocab_size",
+            "width": "hidden_size",
+            "heads": "num_attention_heads",
+            "layers": "num_hidden_layers",
+        },
+        "pooler": "mean_pooler",
     },
-    "pooler": "mean_pooler",
-  }
+    # added BERT
+    "bert": {
+        "config_names": {
+            "context_length": "max_position_embeddings",
+            "vocab_size": "vocab_size",
+            "width": "hidden_size",
+            "heads": "num_attention_heads",
+            "layers": "num_hidden_layers",
+        },
+        "pooler": "mean_pooler",
+    }
 }
